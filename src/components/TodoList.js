8a9308702fc8b60f62ecdcf3017cf8e1d77e3134
popensisa <s.islam.like@mail.ru>
2023-03-06 19:38:29 +0600

    in the middle of lesson

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,26 +1,62 @@
 import { useState } from "react";
 
-const IsEditTo = ({ isEdit, setIsEdit, todo }) => { 
+const IsEditTo = ({ name, isEdit, setIsEdit, title, handleChange, newValue }) => { 
 
     if (isEdit) {
-        return <input placeholder="EDIT"/>
+        return <input name={name} value={newValue} onChange={handleChange} placeholder="EDIT"/>
     } else {
-        return <h1 onClick={() => setIsEdit(true)}>{todo.title}</h1>
+        return <h1 onClick={() => setIsEdit(true)}>{title}</h1>
     }
 }
 
-const TodoList = ({ list, deleteTodo }) => {
+const TodoList = ({ list, deleteTodo, editTodo }) => {
 
     const [ isEdit, setIsEdit ] = useState(false)
-    
+    const [ newValue, setNewValue ] = useState({
+        title: '',
+        desc: ''
+    })
+
+    const handleChange = (e) => {
+        setNewValue({ ...newValue, [ e.target.name ]: e.target.value })
+    }
+
+    const submitNewValue = (todo) => {
+        const newTodo = { ...todo, title: newValue.title, desc: newValue.desc }
+        editTodo(newTodo)
+        setIsEdit(false)
+    }
+
     return ( 
         <div>
-            {list.map((todo) => 
-                    <div key={todo.id}>
-                        <IsEditTo isEdit={isEdit} setIsEdit={setIsEdit} todo={todo}/>
-                        <p>{todo.desc}</p>
-                        <button onClick={() => deleteTodo(todo.id)}>Удалить</button>
-                    </div>
+            {list.map((todo) =>
+                <div key={todo.id}>
+                    <IsEditTo
+                        name='title' 
+                        newValue={newValue.title} 
+                        handleChange={handleChange} 
+                        isEdit={isEdit} 
+                        setIsEdit={setIsEdit} 
+                        title={todo.title}
+                    />
+
+                    <IsEditTo
+                        name='desc' 
+                        newValue={newValue.desc} 
+                        handleChange={handleChange} 
+                        isEdit={isEdit} 
+                        setIsEdit={setIsEdit} 
+                        title={todo.desc}
+                    />
+
+                    {
+                        isEdit
+                            ?
+                                <button onClick={() => submitNewValue(todo)}>Сохранить</button>
+                            :
+                                <button onClick={() => deleteTodo(todo.id)}>Удалить</button>
+                    }
+                </div>
             )}
         </div>
     );
